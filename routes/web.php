b7d88b1b172726728077a4e3aa3567bd96480504
GitHub30 <funaox@gmail.com>
2024-09-08 14:34:56 +0000

    aadd

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@ Route::get('/', function () {
 
 Route::post('/', function () {
     $name = request()->input('name') ?: \Str::random(8);
-    \DB::table('words')->insert(['name' => $name]);
+    $word_id = \DB::table('words')->insertGetId(['name' => $name]);
+    \DB::table('pronunciations')->insert(['word_id' => $word_id, 'name' => $name. '.ok1']);
+    \DB::table('pronunciations')->insert(['word_id' => $word_id, 'name' => $name. '.ok2']);
+    \DB::table('pronunciations')->insert(['word_id' => $word_id, 'name' => $name. '.ok3']);
     $referer = request()->headers->get('referer');
     return redirect($referer);
+});
+
+Route::get('/{word}', function (string $word) {
+    return \DB::table('words')->join('pronunciations', 'pronunciations.word_id', '=', 'words.id')->where('words.name', $word)->get();
 });
