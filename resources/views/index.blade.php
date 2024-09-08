183e55ac5a8c81088bb69dd96623ca199ddf9fa4
GitHub30 <funaox@gmail.com>
2024-09-08 09:35:03 +0000

    add

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YOMIKATA</title>
+</head>
+
+<body>
+    <header>
+        <p>YOMIKATA</p>
+        <p>ちょっと読み方決めようぜ。</p>
+    </header>
+    <nav>
+        <a href="">ホーム</a>
+        <a href="">YOMIKATAとは？</a>
+        <a href="">単語の調べ方</a>
+        <a href="">単語の登録方法</a>
+        <a href="">利用の注意事項</a>
+    </nav>
+    <aside>
+        <form action="">
+            <input type="text">
+            <button>検索</button>
+        </form>
+        <p>単語登録件数:4787件</p>
+        <button>ツイート</button>
+        <details>
+            <summary>a</summary>
+            <p>ここに詳細情報が入ります。</p>
+        </details>
+        <details>
+            <p>ここに詳細情報が入ります。</p>
+        </details>
+    </aside>
+    <main>
+        <p>height</p>
+        <p>orz</p>
+        <p>git</p>
+    </main>
+</body>
+
+</html>
